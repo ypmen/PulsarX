d24338b845c8cdb03d86ec0956d2ba6af83573b2
ypmen <ypmen@mpifr-bonn.mpg.de>
2021-06-18 16:17:40 +0200

    update dmffdot

--- src/pulsar/dmffdot.cpp
+++ src/pulsar/dmffdot.cpp
@@ -10,11 +10,14 @@
 
 #include <boost/program_options.hpp>
 
+#include "logging.h"
 #include "psrfits.h"
 #include "dedisperse.h"
 #include "archivelite.h"
 #include "gridsearch.h"
 #include "pulsarplot.h"
+#include "archivewriter.h"
+#include "constants.h"
 
 using namespace boost::program_options;
 
@@ -22,6 +25,8 @@ unsigned int num_threads = 1;
 
 int main(int argc, char *argv[])
 {
+	init_logging();
+
     /* options */
 	int verbose = 0;
 
@@ -33,6 +38,9 @@ int main(int argc, char *argv[])
 			("scale", value<int>()->default_value(1), "F0,F1,dm search range scale in phase")
 			("nosearch", "Do not search dm,f0,f1")
 			("noplot", "Do not generate figures")
+			("candfile", value<std::vector<std::string>>()->zero_tokens(), "Input candfile and generate a new one")
+			("update", "Update archive")
+			("template", value<std::string>(), "Input fold template file")
 			("srcname", value<std::string>()->default_value("PSRJ0000+00"), "Souce name")
 			("telescope", value<std::string>()->default_value("Fake"), "Telescope name")
 			("ibeam,i", value<int>()->default_value(1), "Beam number")
@@ -45,13 +53,10 @@ int main(int argc, char *argv[])
 			("plotx", "Using PlotX for plotting")
 #endif
 			("rootname,o", value<std::string>()->default_value("J0000-00"), "Output rootname")
-			("input,f", value<std::string>()->multitoken()->composing(), "Input files");
+			("input,f", value<std::vector<std::string>>()->multitoken()->composing(), "Input files");
 
-    positional_options_description pos_desc;
-    pos_desc.add("input", -1);
     command_line_parser parser{argc, argv};
     parser.options(desc).style(command_line_style::default_style | command_line_style::allow_short);
-    parser.options(desc).positional(pos_desc);
     parsed_options parsed_options = parser.run();
 
     variables_map vm;
@@ -69,8 +74,16 @@ int main(int argc, char *argv[])
 	}
 	if (vm.count("input") == 0)
 	{
-		cerr<<"Error: no input file"<<endl;
+		BOOST_LOG_TRIVIAL(error)<<"Error: no input file";
 		return -1;
+	}
+	if (vm.count("update"))
+	{
+		if (vm.count("template") == 0)
+		{
+			BOOST_LOG_TRIVIAL(error)<<"Error: no template file";
+			return -1;
+		}
 	}
 
 	int scale = vm["scale"].as<int>();
@@ -81,12 +94,12 @@ int main(int argc, char *argv[])
 	string src_name = vm["srcname"].as<std::string>();
 	string s_telescope = vm["telescope"].as<std::string>();
     num_threads = vm["threads"].as<unsigned int>();
-    std::string fname = vm["input"].as<std::string>();
+    std::vector<std::string> fnames = vm["input"].as<std::vector<std::string>>();
 	double src_raj = vm["ra"].as<double>();
 	double src_dej = vm["dec"].as<double>();
 
 	Psrfits psf;
-	psf.filename = fname;
+	psf.filename = fnames[0];
     psf.open();
     psf.primary.load(psf.fptr);
     psf.load_mode();
@@ -132,109 +145,10 @@ int main(int argc, char *argv[])
 
     psf.close();
 
-    /* read archive */
-    Pulsar::ArchiveLite arch;
-    arch.read_archive(fname);
-
-    double tint = 0;
-    for (auto it=arch.profiles.begin(); it!=arch.profiles.end(); ++it)
-    {
-        tint += it->tsubint;
-    }
-
-    double dm = arch.dm;
-	double f0 = arch.f0;
-	double f1 = arch.f1;
-    double fcentre = 0.5*(arch.frequencies.front()+arch.frequencies.back());
-    double bandwidth = (arch.frequencies.back()-arch.frequencies.front())/(arch.frequencies.size()-1)*arch.frequencies.size();
-    double fmax = std::max(fcentre-0.5*bandwidth, fcentre+0.5*bandwidth);
-    double fmin = std::min(fcentre-0.5*bandwidth, fcentre+0.5*bandwidth);
-
-    /* optimize */
-    Pulsar::GridSearch gridsearch;
-    gridsearch.ddmstart = -scale*1./f0/Pulsar::DedispersionLite::dmdelay(1, fmax, fmin);
-    gridsearch.ddmstep = 1./scale*abs(gridsearch.ddmstart/arch.nbin);
-    gridsearch.nddm = 2*abs(scale)*arch.nbin;
-    gridsearch.df0start = -scale*1./tint;
-    gridsearch.df0step = 1./scale*abs(gridsearch.df0start/arch.nbin);
-    gridsearch.ndf0 = 2*abs(scale)*arch.nbin;
-    gridsearch.df1start = -scale*2./(tint*tint);
-    gridsearch.df1step = 1./scale*abs(gridsearch.df1start/arch.nbin);
-    gridsearch.ndf1 = 2*abs(scale)*arch.nbin;
-
-    gridsearch.clfd_q = vm["clfd"].as<double>();
-
-    gridsearch.prepare(arch);
-
-    if (!nosearch)
-    {
-        double dm0 = gridsearch.dm;
-        double f00 = gridsearch.f0;
-        double f10 = gridsearch.f1;
-        double dm1 = gridsearch.dm + 2*gridsearch.ddmstep;
-        double f01 = gridsearch.f0 + 2*gridsearch.df0step;
-        double f11 = gridsearch.f1 + 2*gridsearch.df1step;
-        int cont = 0;
-        //cout<<gridsearch.dm<<" "<<gridsearch.f0<<" "<<gridsearch.f1<<endl;
-        while ((abs(dm0-dm1)>gridsearch.ddmstep or abs(f00-f01)>gridsearch.df0step or abs(f10-f11)>gridsearch.df1step) and cont<8)
-        {
-            dm0 = gridsearch.dm;
-            f00 = gridsearch.f0;
-            f10 = gridsearch.f1;
-
-            gridsearch.runFFdot();
-            gridsearch.bestprofiles();
-            gridsearch.runDM();
-            gridsearch.bestprofiles();
-
-            dm1 = gridsearch.dm;
-            f01 = gridsearch.f0;
-            f11 = gridsearch.f1;
-
-            cont++;
-            //cout<<gridsearch.dm<<" "<<gridsearch.f0<<" "<<gridsearch.f1<<endl;
-        }
-    }
-
-    /**
-     * @brief recalculate the mxsbr_ffdot and vsnr_dm with scale = 3 for plotting
-     * 
-     */
-
-    dm = gridsearch.dm;
-    f0 = gridsearch.f0;
-    f1 = gridsearch.f1;
-
-    double ddmstart = -3*1./f0/Pulsar::DedispersionLite::dmdelay(1, fmax, fmin);
-    gridsearch.ddmstep = 1./3*abs(ddmstart/arch.nbin);
-    int nddm = 2*3*arch.nbin;
-
-    gridsearch.ddmstart = std::max(-3*1./f0/Pulsar::DedispersionLite::dmdelay(1, fmax, fmin), -dm);
-    gridsearch.nddm = (ddmstart+gridsearch.ddmstep*nddm-gridsearch.ddmstart)/gridsearch.ddmstep;
-
-    gridsearch.df0start = -3*1./tint;
-    gridsearch.df0step = 1./3*abs(gridsearch.df0start/arch.nbin);
-    gridsearch.ndf0 = 2*3*arch.nbin;
-    gridsearch.df1start = -3*2./(tint*tint);
-    gridsearch.df1step = 1./3*abs(gridsearch.df1start/arch.nbin);
-    gridsearch.ndf1 = 2*3*arch.nbin;
-
-    if (!nosearch)
-    {
-        gridsearch.runFFdot();
-        gridsearch.runDM();
-    }
-
-
-    /** form obsinfo*/
+	/** form obsinfo*/
 	std::map<std::string, std::string> obsinfo;
 	//source name
 	obsinfo["Source_name"] = src_name;
-	//start mjd
-	stringstream ss_mjd;
-    ss_mjd << setprecision(10) << fixed << arch.start_mjd.to_day();
-    string s_mjd = ss_mjd.str();
-	obsinfo["Date"] = s_mjd;
 	//ra dec string
 	string s_ra, s_dec;
 	get_s_radec(src_raj, src_dej, s_ra, s_dec);
@@ -245,19 +159,12 @@ int main(int argc, char *argv[])
 	//beam
 	stringstream ss_ibeam;
 	if (vm.count("incoherent"))
-    	ss_ibeam << "ifbf" << setw(5) << setfill('0') << ibeam;
+		ss_ibeam << "ifbf" << setw(5) << setfill('0') << ibeam;
 	else
 		ss_ibeam << "cfbf" << setw(5) << setfill('0') << ibeam;
 	string s_ibeam = ss_ibeam.str();
 	obsinfo["Beam"] = s_ibeam;
-	//data filename
-	obsinfo["Filename"] = fname;
-	//observation length
-	obsinfo["Obslen"] = to_string(tint);
-    //frequency
-    obsinfo["Fcentre"] = to_string(fcentre);
-    obsinfo["Bandwidth"] = to_string(bandwidth);
-    obsinfo["Nchan"] = to_string(1000000);
+	obsinfo["Nchan"] = to_string(1000000);
 
 	double gl = 0., gb = 0.;
 #ifdef HAVE_SOFA
@@ -271,30 +178,257 @@ int main(int argc, char *argv[])
 
 	obsinfo["MaxDM_YMW16"] = to_string(ymw16_maxdm);
 
-	double pepoch_offset = arch.profiles[arch.profiles.size()/2].offs_sub;
-	
-	//pepoch
-	stringstream ss_pepoch;
-    ss_pepoch << setprecision(9) << fixed << (arch.start_mjd+pepoch_offset).to_day();
-    string s_pepoch = ss_pepoch.str();
-	obsinfo["Pepoch"] = s_pepoch;
+	BOOST_LOG_TRIVIAL(info)<<std::endl
+	<<"Source name: "<<obsinfo["Source_name"]<<std::endl
+	<<"RA: "<<obsinfo["RA"]<<std::endl
+	<<"DEC: "<<obsinfo["DEC"]<<std::endl
+	<<"Telescope: "<<obsinfo["Telescope"]<<std::endl
+	<<"Beam: "<<obsinfo["Beam"];
 
-    gridsearch.get_snr_width();
-	gridsearch.get_error(obsinfo);
+	std::ofstream outfile;
+	if (vm.count("candfile"))
+	{
+		if (vm["candfile"].as<std::vector<std::string>>().size() != 0)
+		{
+			BOOST_LOG_TRIVIAL(info)<<"read header of input candfile...";
 
-    double ymw16_dist = get_dist_ymw16(gl, gb, gridsearch.dm);
+			std::ifstream infile(vm["candfile"].as<std::vector<std::string>>()[0]);
 
-    if (!noplot)
-    {
-        obsinfo["Dist_YMW16"] = to_string(ymw16_dist);
+			std::vector<std::string> header;
+			std::string line;
+			while (getline(infile, line))
+			{
+				boost::trim(line);
+				if(line.rfind("#", 0) == 0)
+				{
+					header.push_back(line);
+				}
+			}
 
-		size_t lastindex = fname.find_last_of("."); 
-		std::string basename = fname.substr(0, lastindex);
-		obsinfo["Basename"] = basename;
+			infile.close();
 
-        Pulsar::PulsarPlot psrplot;
-        psrplot.plot(arch, gridsearch, obsinfo, 1, rootname, vm.count("plotx"));
-    }
+			BOOST_LOG_TRIVIAL(info)<<"write header of ouput candfile...";
+			outfile.open(vm["candfile"].as<std::string>());
+			for (auto line=header.begin(); line!=header.end(); ++line)
+			{
+				outfile<<*line<<std::endl;
+			}
+		}
+		else
+		{
+			BOOST_LOG_TRIVIAL(info)<<"write header of ouput candfile...";
+
+			stringstream ss_mjd;
+			ss_mjd << setprecision(10) << fixed << psf.primary.start_mjd.to_day();
+			string s_mjd = ss_mjd.str();
+
+			outfile.open(rootname + "_" + s_mjd + "_" + s_ibeam + ".cands");
+
+			outfile<<"#Filename "<<"--"<<endl;
+			outfile<<"#Telescope "<<obsinfo["Telescope"]<<endl;
+			outfile<<"#Source_name "<<obsinfo["Source_name"]<<endl;
+			outfile<<"#Beam "<<obsinfo["Beam"]<<endl;
+			outfile<<"#Date "<<s_mjd<<endl;
+			outfile<<"#RA "<<obsinfo["RA"]<<endl;
+			outfile<<"#DEC "<<obsinfo["DEC"]<<endl;
+			outfile<<"#GL "<<obsinfo["GL"]<<endl;
+			outfile<<"#GB "<<obsinfo["GB"]<<endl;
+			outfile<<"#MaxDM_YMW16 "<<obsinfo["MaxDM_YMW16"]<<endl;
+			outfile<<"#Pepoch "<<"--"<<endl;
+			outfile<<"#id       dm_old      dm_new      dm_err		dist_ymw16     f0_old     f0_new        f0_err      f1_old     f1_new       f1_err      acc_old        acc_new      acc_err      S/N        S/N_new"<<endl;
+		}
+	}
+
+	long int narch = fnames.size();
+
+	BOOST_LOG_TRIVIAL(info)<<"read "<<narch<<" archives...";
+
+    /* read archive */
+	for (long int k=0; k<narch; k++)
+	{
+		std::string fname = fnames[k];
+
+		BOOST_LOG_TRIVIAL(info)<<"read "<<fname;
+
+		Pulsar::ArchiveLite arch;
+
+		arch.read_archive(fname);
+
+		double tint = 0.;
+		for (auto it=arch.profiles.begin(); it!=arch.profiles.end(); ++it)
+		{
+			tint += it->tsubint;
+		}
+
+		double dm = arch.dm;
+		double f0 = arch.f0;
+		double f1 = arch.f1;
+		double fcentre = 0.5*(arch.frequencies.front()+arch.frequencies.back());
+		double bandwidth = (arch.frequencies.back()-arch.frequencies.front())/(arch.frequencies.size()-1)*arch.frequencies.size();
+		double fmax = std::max(fcentre-0.5*bandwidth, fcentre+0.5*bandwidth);
+		double fmin = std::min(fcentre-0.5*bandwidth, fcentre+0.5*bandwidth);
+
+		BOOST_LOG_TRIVIAL(info)<<"inital DM (pc/cc)="<<dm<<", f0 (Hz)="<<f0<<" ,f1 (Hz/s)="<<f1;
+
+		/* optimize */
+		Pulsar::GridSearch gridsearch;
+		gridsearch.ddmstart = -scale*1./f0/Pulsar::DedispersionLite::dmdelay(1, fmax, fmin);
+		gridsearch.ddmstep = 1./scale*abs(gridsearch.ddmstart/arch.nbin);
+		gridsearch.nddm = 2*abs(scale)*arch.nbin;
+		gridsearch.df0start = -scale*1./tint;
+		gridsearch.df0step = 1./scale*abs(gridsearch.df0start/arch.nbin);
+		gridsearch.ndf0 = 2*abs(scale)*arch.nbin;
+		gridsearch.df1start = -scale*2./(tint*tint);
+		gridsearch.df1step = 1./scale*abs(gridsearch.df1start/arch.nbin);
+		gridsearch.ndf1 = 2*abs(scale)*arch.nbin;
+
+		gridsearch.clfd_q = vm["clfd"].as<double>();
+
+		gridsearch.prepare(arch);
+
+		BOOST_LOG_TRIVIAL(info)<<"optimization...";
+
+		if (!nosearch)
+		{
+			double dm0 = gridsearch.dm;
+			double f00 = gridsearch.f0;
+			double f10 = gridsearch.f1;
+			double dm1 = gridsearch.dm + 2*gridsearch.ddmstep;
+			double f01 = gridsearch.f0 + 2*gridsearch.df0step;
+			double f11 = gridsearch.f1 + 2*gridsearch.df1step;
+			int cont = 0;
+			BOOST_LOG_TRIVIAL(debug)<<gridsearch.dm<<" "<<gridsearch.f0<<" "<<gridsearch.f1;
+			while ((abs(dm0-dm1)>gridsearch.ddmstep or abs(f00-f01)>gridsearch.df0step or abs(f10-f11)>gridsearch.df1step) and cont<8)
+			{
+				dm0 = gridsearch.dm;
+				f00 = gridsearch.f0;
+				f10 = gridsearch.f1;
+
+				gridsearch.runFFdot();
+				gridsearch.bestprofiles();
+				gridsearch.runDM();
+				gridsearch.bestprofiles();
+
+				dm1 = gridsearch.dm;
+				f01 = gridsearch.f0;
+				f11 = gridsearch.f1;
+
+				cont++;
+				BOOST_LOG_TRIVIAL(debug)<<gridsearch.dm<<" "<<gridsearch.f0<<" "<<gridsearch.f1;
+			}
+		}
+
+		/**
+		 * @brief recalculate the mxsbr_ffdot and vsnr_dm with scale = 3 for plotting
+		 * 
+		 */
+
+		dm = gridsearch.dm;
+		f0 = gridsearch.f0;
+		f1 = gridsearch.f1;
+
+		double ddmstart = -3*1./f0/Pulsar::DedispersionLite::dmdelay(1, fmax, fmin);
+		gridsearch.ddmstep = 1./3*abs(ddmstart/arch.nbin);
+		int nddm = 2*3*arch.nbin;
+
+		gridsearch.ddmstart = std::max(-3*1./f0/Pulsar::DedispersionLite::dmdelay(1, fmax, fmin), -dm);
+		gridsearch.nddm = (ddmstart+gridsearch.ddmstep*nddm-gridsearch.ddmstart)/gridsearch.ddmstep;
+
+		gridsearch.df0start = -3*1./tint;
+		gridsearch.df0step = 1./3*abs(gridsearch.df0start/arch.nbin);
+		gridsearch.ndf0 = 2*3*arch.nbin;
+		gridsearch.df1start = -3*2./(tint*tint);
+		gridsearch.df1step = 1./3*abs(gridsearch.df1start/arch.nbin);
+		gridsearch.ndf1 = 2*3*arch.nbin;
+
+		if (!nosearch)
+		{
+			gridsearch.runFFdot();
+			gridsearch.runDM();
+		}
+
+		//start mjd
+		stringstream ss_mjd;
+		ss_mjd << setprecision(10) << fixed << arch.start_mjd.to_day();
+		string s_mjd = ss_mjd.str();
+		obsinfo["Date"] = s_mjd;
+
+		//data filename
+		obsinfo["Filename"] = fname;
+		//observation length
+		obsinfo["Obslen"] = to_string(tint);
+		//frequency
+		obsinfo["Fcentre"] = to_string(fcentre);
+		obsinfo["Bandwidth"] = to_string(bandwidth);
+
+		double pepoch_offset = arch.profiles[arch.profiles.size()/2].offs_sub;
+
+		stringstream ss_pepoch;
+		ss_pepoch << setprecision(9) << fixed << (arch.start_mjd+pepoch_offset).to_day();
+		string s_pepoch = ss_pepoch.str();
+		obsinfo["Pepoch"] = s_pepoch;
+
+		if (vm.count("update"))
+		{
+			BOOST_LOG_TRIVIAL(info)<<"update archive...";
+			size_t lastindex = fname.find_last_of("."); 
+			std::string basename = fname.substr(0, lastindex);
+
+			ArchiveWriter writer;
+			writer.template_file = vm["template"].as<string>();
+			writer.mode = Integration::FOLD;
+			writer.ibeam = 1;
+			writer.src_name = src_name;
+			writer.ra = s_ra;
+			writer.dec = s_dec;
+			writer.rootname = basename;
+
+			writer.prepare(arch, gridsearch);
+			writer.run(arch, gridsearch);
+		}
+
+		gridsearch.get_snr_width();
+		gridsearch.get_error(obsinfo);
+
+		double ymw16_dist = get_dist_ymw16(gl, gb, gridsearch.dm);
+
+		if (vm.count("candfile"))
+		{
+			outfile<<k+1<<"\t\t";
+			outfile<<fixed<<setprecision(8)<<arch.dm<<"\t\t";
+			outfile<<fixed<<setprecision(8)<<gridsearch.dm<<"\t\t";
+			outfile<<setprecision(15)<<gridsearch.err_dm<<"\t\t";
+			outfile<<fixed<<setprecision(1)<<ymw16_dist<<"\t\t";
+			outfile<<setprecision(15)<<arch.f0<<"\t\t";
+			outfile<<setprecision(15)<<gridsearch.f0<<"\t\t";
+			outfile<<setprecision(15)<<gridsearch.err_f0<<"\t\t";
+			outfile<<setprecision(15)<<arch.f1<<"\t\t";
+			outfile<<setprecision(15)<<gridsearch.f1<<"\t\t";
+			outfile<<setprecision(15)<<gridsearch.err_f1<<"\t\t";
+			outfile<<setprecision(15)<<-arch.f1/arch.f0*CONST_C<<"\t\t";
+			outfile<<setprecision(15)<<gridsearch.acc<<"\t\t";
+			outfile<<setprecision(15)<<gridsearch.err_acc<<"\t\t";
+			outfile<<fixed<<setprecision(5)<<arch.snr<<"\t\t";
+			outfile<<fixed<<setprecision(5)<<gridsearch.snr<<endl;
+		}
+
+		if (!noplot)
+		{
+			BOOST_LOG_TRIVIAL(info)<<"plotting...";
+			obsinfo["Dist_YMW16"] = to_string(ymw16_dist);
+
+			size_t lastindex = fname.find_last_of("."); 
+			std::string basename = fname.substr(0, lastindex);
+			obsinfo["Basename"] = basename;
+
+			Pulsar::PulsarPlot psrplot;
+			psrplot.plot(arch, gridsearch, obsinfo, 1, rootname, vm.count("plotx"));
+		}
+	}
+
+	outfile.close();
+
+	BOOST_LOG_TRIVIAL(info)<<"done!";
 
     return 0;
 }
