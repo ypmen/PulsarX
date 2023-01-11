c033f34d50f0f3d21a2e8a5e2e6ed9ed3ab635ab
ypmen <ypmen@mpifr-bonn.mpg.de>
2023-01-11 18:00:52 +0100

    fixed Dockerfile

--- python/setup.py
+++ python/setup.py
@@ -16,9 +16,9 @@ ext_modules = [
                  "../src/utils/utils.cpp"
         ],
         extra_compile_args=["-O3", "-mavx2", "-mfma"],
-        library_dirs=["../lib/"],
+        library_dirs=["../../lib/", "../../sofa/20200721/c/src"],
         libraries=["ymw16", "sofa_c", "boost_log", "fftw3f_threads", "fftw3f", "fftw3_threads", "fftw3"],
-        include_dirs=["../include", "../"]
+        include_dirs=["../include", "../", "../../sofa/20200721/c/src"]
     ),
     Pybind11Extension(
         "psrdata_reader",
@@ -35,14 +35,15 @@ ext_modules = [
 		 "../src/utils/utils.cpp"
 	],
 	extra_compile_args=["-O3", "-mavx2", "-mfma"],
-	library_dirs=["../lib/"],
+	library_dirs=["../../lib/", "../../sofa/20200721/c/src"],
 	libraries=["cfitsio", "ymw16", "sofa_c", "boost_log", "fftw3f_threads", "fftw3f", "fftw3_threads", "fftw3"],
-	include_dirs=["../include", "../"]
+	include_dirs=["../include", "../", "../../sofa/20200721/c/src"]
     ),
     Pybind11Extension(
         "orbit_utils",
         sources=["cpp/orbit_utils.cpp"],
-	extra_compile_args=["-O3", "-mavx2"],
+		extra_compile_args=["-O3", "-mavx2"],
+		libraries=["lapack"],
         include_dirs=["../include", "../"]
     ),
     Pybind11Extension(
